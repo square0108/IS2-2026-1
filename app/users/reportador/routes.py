83979f7e4294991ec43b01b5697d724eafba5d56
square0108 <martinmariollanos@gmail.com>
2026-05-26 15:27:17 -0400

    fix: Manejar username en vez de email en templates, pueden ser intercompatibles igual

--- app/users/reportador/routes.py
+++ app/users/reportador/routes.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, render_template
+from app.auth.login_required import login_required
 
 reportador = Blueprint('reportador', __name__)
 
 @reportador.route('/')
+@login_required("reportador")
 def home():
   return render_template('reporter_home.html')
 
 @reportador.route('/reportarIncidente', methods=["GET"])
+@login_required("reportador")
 def nuevoReporte():
   return render_template('components/search_students.html') # por ahora solo el buscador
