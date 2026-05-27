91e414eaeeb9f0e6415916b5f85ca74a28160460
Krozzito <crfigueroa2021@udec.cl>
2026-05-26 23:21:39 -0400

    Función de consultas
    Se implementó una función de consultas

--- app/users/reportador/routes.py
+++ app/users/reportador/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify, abort
 from app.auth.login_required import login_required
+from app.queries import listar_consultas, ejecutar_consulta
 
 reportador = Blueprint('reportador', __name__)
 
@@ -12,3 +13,18 @@ def home():
 @login_required("reportador")
 def nuevoReporte():
   return render_template('components/search_students.html') # por ahora solo el buscador
+
+@reportador.route('/consultas', methods=["GET"])
+@login_required("reportador")
+def listar_consultas_route():
+    return jsonify(listar_consultas())
+
+@reportador.route('/consultas/run', methods=["GET"])
+@login_required("reportador")
+def ejecutar_consulta_route():
+    tipo = request.args.get("tipo")
+    if not tipo:
+        abort(400)
+
+    resultados = ejecutar_consulta(tipo, request.args.to_dict())
+    return jsonify(resultados)
