ad242be87654991282ed3656b4cf39de555d33ed
1shiro <nose.x2xd@gmail.com>
2026-05-29 10:24:08 -0400

    terminacion de conecciones de interfaz del expediente de usuario + cambios en el login required para ajustar 'permisos'al volver a paginas.

--- app/auth/login_required.py
+++ app/auth/login_required.py
@@ -2,27 +2,59 @@ from functools import wraps
 from flask import session, redirect, url_for, flash
 
 def login_required(user_type):
-    """ 
-    Verifica que el tipo de usuario sea el que tiene permiso para acceder a la ruta. Si no,
-    se redirige a la ruta de inicio de sesión.
 
-    Parameters:
-    - user_type: tipo de usuario que puede acceder estos pueden ser: 'reportador', 'encargado_de_convivencia' o 'orientador'
-
-    Returns:
-    - redirect(url_for('auth.signin')), si el usuario no tiene permisos para acceder a la página
-    - nada, si el usuario si puede acceder al sitio.
     """
-    
+    Verifica que el usuario haya iniciado sesión y tenga permisos
+    para acceder a una ruta específica.
+    """
+
     def decorator(func):
+
         @wraps(func)
         def wrapper(*args, **kwargs):
+
+            # Usuario no autenticado
             if 'user_id' not in session:
-                flash('Debe iniciar sesión para acceder a esta página.', 'warning')
+
+                flash(
+                    'Debe iniciar sesión para acceder a esta página.',
+                    'warning'
+                )
+
                 return redirect(url_for('auth.signin'))
+
+            # Usuario autenticado pero sin permisos
             elif session['user_type'] != user_type:
-                flash('No tiene permiso para acceder a esta página.', 'warning')
+
+                flash(
+                    'No tiene permiso para acceder a esta página.',
+                    'warning'
+                )
+
+                # Redirige segun el rol
+
+                if session['user_type'] == "reportador":
+
+                    return redirect(
+                        url_for('reportador.home')
+                    )
+
+                elif session['user_type'] == "encargado_de_convivencia":
+
+                    return redirect(
+                        url_for('encargado_de_convivencia.home')
+                    )
+
+                elif session['user_type'] == "orientador":
+
+                    return redirect(
+                        url_for('orientador.home')
+                    )
+
                 return redirect(url_for('auth.signin'))
+
             return func(*args, **kwargs)
+
         return wrapper
+
     return decorator
