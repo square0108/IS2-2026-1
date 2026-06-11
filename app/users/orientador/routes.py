c25d58459e10a35503019f8451530418a5df1eda
square0108 <martinmariollanos@gmail.com>
2026-06-11 17:59:52 -0400

    Refactor para compatibilizar con estructura de carpetas actual

--- app/users/orientador/routes.py
+++ app/users/orientador/routes.py
@@ -7,31 +7,31 @@ orientador = Blueprint('orientador', __name__)
 @orientador.route('/')
 @login_required("orientador")
 def home():
-    return render_template('orientador_home.html')
+    return render_template('orientador/orientador_home.html')
 
 
 @orientador.route('/reportarIncidente')
 @login_required("orientador")
 def nuevoReporte():
-    return render_template('components/search_students.html')
+    return render_template('shared_components/search_students.html')
 
 
 @orientador.route('/explorarIncidentes')
 @login_required("orientador")
 def explorarIncidentes():
-    return render_template('explore_incidents.html')
+    return render_template('encargado_de_convivencia/explorar_antecedentes.html')
 
 
 @orientador.route('/misCasos')
 @login_required("orientador")
-def misCasos():
+def misCasos(): # No recuerdo si decidimos que este tipo de usuario podía ver todos los casos
     casos = Caso.query.order_by(Caso.fecha_creacion.desc()).all()
 
     casos_abiertos = [c for c in casos if c.estado == "ABIERTO"]
     casos_cerrados = [c for c in casos if c.estado != "ABIERTO"]
 
     return render_template(
-        'my_cases.html',
+        'encargado_de_convivencia/mis_casos.html',
         casos_abiertos=casos_abiertos,
         casos_cerrados=casos_cerrados
     )
@@ -40,10 +40,10 @@ def misCasos():
 @orientador.route('/nuevoCaso')
 @login_required("orientador")
 def nuevoCaso():
-    return render_template('new_case.html')
+    return render_template('encargado_de_convivencia/caso_nuevo.html')
 
 
 @orientador.route('/diagnostico')
 @login_required("orientador")
 def diagnostico():
-    return render_template('diagnostico.html')
+    return render_template('orientador/diagnostico.html')
